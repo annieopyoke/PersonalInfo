4a0f47e432e92a0c63c20b96d21164ce55337b8b
annieopyoke <annieopyoke@gmail.com>
2017-11-08 19:35:39 -0500

    personalinfo

---
+++ personalinfo.rb
@@ -0,0 +1,25 @@
+def age()
+    puts "How old are you?"
+    age = gets.chomp
+    age.to_i
+
+end
+
+def birthMonth()
+    puts "when is your birthday month?"
+    month = gets.chomp
+    month
+    
+
+end
+
+
+
+
+
+
+
+# def birthMonth()
+# puts "What month were you born in?"
+# month = gets.to
+# end
